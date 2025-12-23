c2641d3523c3f9e82ba5d2a282dd4fc466c80bc6
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-23 15:07:39 +0530

    h

--- src/components/MultiStepForm.js
+++ src/components/MultiStepForm.js
@@ -7,6 +7,22 @@ import Step from "./Step";
 const MultiStepForm = () => {
   const[step,setStep]=useState(1)
 
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    model: "",
+    car_price: "",
+    card_info: "",
+    expiry_date: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData(prev => ({
+      ...prev,
+      [e.target.id]: e.target.value
+    }));
+  };
+
   const handlenext=()=>{
     if(step<3){
       setStep(step+1)
@@ -20,13 +36,15 @@ const MultiStepForm = () => {
   }
 
   const handlesubmit=()=>{
-    if(step===3){
+    
       console.log("Form submitted");
-    }
+    
   }
   return (
     
       <Step step={step}
+      data={formData}
+      onChange={handleChange}
       handlenext={handlenext}
       handleprevious={handleprevious}
       handlesubmit={handlesubmit}
--- src/components/Step.js
+++ src/components/Step.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Step = ({step,handlenext,handleprevious,handlesubmit}) => {
+const Step = ({step, data, onChange,handlenext,handleprevious,handlesubmit}) => {
   return (
     <>
         {step===1 &&
@@ -10,11 +10,15 @@ const Step = ({step,handlenext,handleprevious,handlesubmit}) => {
             <input
             id="first_name"
             type="text"
+            value={data.first_name}
+            onChange={onChange}
             /><br></br>
             <label htmlFor="last_name">Last Name:</label>
             <input
             type="text"
             id="last_name"
+             value={data.last_name}
+            onChange={onChange}
             /><br></br>
             <button type="button" onClick={handlenext}>Next</button>
         </form>
@@ -26,10 +30,14 @@ const Step = ({step,handlenext,handleprevious,handlesubmit}) => {
                 <label htmlFor="model">Model:</label>
                 <input
                 id="model"
+                value={data.model}
+            onChange={onChange}
                 /><br></br>
                 <label htmlFor="car_price">Price:</label>
                 <input
                 id="car_price"
+                value={data.car_price}
+            onChange={onChange}
                 /><br></br>
                 <button type="button" onClick={handleprevious}>Previous</button>
                 <button type="button" onClick={handlenext}>Next</button>
@@ -42,11 +50,15 @@ const Step = ({step,handlenext,handleprevious,handlesubmit}) => {
                 <label htmlFor="card_info">Credit Card Number:</label>
                 <input
                 id="card_info"
+                value={data.card_info}
+            onChange={onChange}
                 /><br></br>
                 <label htmlFor="expiry_date">Expiration Date:</label>
                 <input
                 id="expiry_date"
                 type="month"
+            value={data.expiry_date}
+            onChange={onChange}
                 /><br></br>
                 <button type="button" onClick={handleprevious}>Previous</button>
                 <button type="button" onClick={handlesubmit}>Submit</button>
