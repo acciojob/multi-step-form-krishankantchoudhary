59bb4bc93978d968505cfd4d856cd08812e40fc6
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-23 17:53:38 +0530

    re

--- src/components/Step.js
+++ src/components/Step.js
@@ -1,71 +1,117 @@
-import React from 'react'
+import React from "react";
 
-const Step = ({step, data, onChange,handlenext,handleprevious,handlesubmit}) => {
+const Step = ({
+  step,
+  data,
+  onChange,
+  handlenext,
+  handleprevious,
+  handlesubmit
+}) => {
   return (
     <>
-        {step===1 &&
+      {/* STEP 1 */}
+      {step === 1 && (
         <form id="step1">
-            <h2>Customer Details</h2>
-            <label htmlFor="first_name">First Name:</label>
-            <input
+          <h2>Customer Details</h2>
+
+          <label htmlFor="first_name">First Name:</label>
+          <input
             id="first_name"
             type="text"
             value={data.first_name}
             onChange={onChange}
-            /><br></br>
-            <label htmlFor="last_name">Last Name:</label>
-            <input
-            type="text"
+          />
+
+          <br />
+
+          <label htmlFor="last_name">Last Name:</label>
+          <input
             id="last_name"
-             value={data.last_name}
+            type="text"
+            value={data.last_name}
             onChange={onChange}
-            /><br></br>
-            <button type="button" onClick={handlenext}>Next</button>
+          />
+
+          <br />
+
+          <button type="button" onClick={handlenext}>
+            Next
+          </button>
         </form>
-        }
-        {
-            step===2 &&
-            <form id="step2">
-                <h2>Car Details</h2>
-                <label htmlFor="model">Model:</label>
-                <input
-                id="model"
-                value={data.model}
+      )}
+
+      {/* STEP 2 */}
+      {step === 2 && (
+        <form id="step2">
+          <h2>Car Details</h2>
+
+          <label htmlFor="model">Model:</label>
+          <input
+            id="model"
+            type="text"
+            value={data.model}
             onChange={onChange}
-                /><br></br>
-                <label htmlFor="car_price">Price:</label>
-                <input
-                id="car_price"
-                value={data.car_price}
+          />
+
+          <br />
+
+          <label htmlFor="car_price">Price:</label>
+          <input
+            id="car_price"
+            type="text"
+            value={data.car_price}
             onChange={onChange}
-                /><br></br>
-                <button type="button" onClick={handleprevious}>Previous</button>
-                <button type="button" onClick={handlenext}>Next</button>
-            </form>
-        }
-        {
-            step===3 &&
-            <form id="step3">
-                <h2>Payment Details</h2>
-                <label htmlFor="card_info">Credit Card Number:</label>
-                <input
-                id="card_info"
-                value={data.card_info}
+          />
+
+          <br />
+
+          <button type="button" onClick={handleprevious}>
+            Previous
+          </button>
+
+          <button type="button" onClick={handlenext}>
+            Next
+          </button>
+        </form>
+      )}
+
+      {/* STEP 3 */}
+      {step === 3 && (
+        <form id="step3">
+          <h2>Payment Details</h2>
+
+          <label htmlFor="card_info">Credit Card Number:</label>
+          <input
+            id="card_info"
+            type="text"
+            value={data.card_info}
             onChange={onChange}
-                /><br></br>
-                <label htmlFor="expiry_date">Expiration Date:</label>
-                <input
-                id="expiry_date"
-                type="month"
+          />
+
+          <br />
+
+          <label htmlFor="expiry_date">Expiration Date:</label>
+          <input
+            id="expiry_date"
+            type="month"
             value={data.expiry_date}
             onChange={onChange}
-                /><br></br>
-                <button type="button" onClick={handleprevious}>Previous</button>
-                <button type="button" onClick={handlesubmit}>Submit</button>
-            </form>
-        }
+          />
+
+          <br />
+
+          <button type="button" onClick={handleprevious}>
+            Previous
+          </button>
+
+          <button type="button" onClick={handlesubmit}>
+            Submit
+          </button>
+        </form>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default Step;
